ffd2e38b263e8a2835d899695095730fa0d80d35
Raquel-Fernandes <fernandessantos.r1@gmail.com>
2025-11-12 21:25:52 -0300

    feat: add model classes

--- src/main/java/com/portfolio/model/Address.java
+++ src/main/java/com/portfolio/model/Address.java
@@ -1,4 +1,13 @@
 package com.portfolio.model;
 
+import java.util.Date;
+
 public class Address {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private Date birth_date;
+
 }
--- src/main/java/com/portfolio/model/Contact.java
+++ src/main/java/com/portfolio/model/Contact.java
@@ -1,4 +1,10 @@
 package com.portfolio.model;
 
 public class Contact {
+    private Integer id;
+    private Integer contact_id;
+    private String street;
+    private Integer number;
+    private String zip_code;
+
 }
